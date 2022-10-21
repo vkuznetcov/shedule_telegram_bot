b409092d84bf771bddeb23994e7ed602231b6c0d
slav <vkuznetcov48@gmail.com>
2022-10-21 18:41:42 +0400

    created cells editing logic

--- bot_core.py
+++ bot_core.py
@@ -2,11 +2,21 @@ import telebot
 import re
 import spreadsheet
 from telebot import types
+from enum import Enum
+
+
+class Modes(Enum):
+    MOVE = 'move'
+    CANCEL = 'cancel'
+    NONE = 'none'
+
 
 token = '5692398615:AAFeVNfA4UZGbTE0-hJY9rexq2AI2DCF2Y4'
 bot = telebot.TeleBot(token)
 time_pattern = re.compile('^\d{0,2}:\d{2} мск$')
+entry_pattern = re.compile('^[А-я]{5,11} \d{0,2}:\d{2} мск$')
 cur_weekday = None
+mode = Modes.NONE
 
 
 def weekdays_markup():
@@ -19,46 +29,117 @@ def weekdays_markup():
                , types.KeyboardButton("Суббота"))
     return markup
 
+def create_entry(message):
+    markup = weekdays_markup()
+    markup.add(types.KeyboardButton('Отмена'))
+    bot.send_message(message.chat.id, "Выберите день для новой записи 👇", reply_markup=markup)
+    print(message.chat.id)
 
 @bot.message_handler(commands=['start'])
 def start_message(message, hello_message=True):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add(types.KeyboardButton('Записаться на занятие'))
-    # markup.add(types.KeyboardButton('Изменить запись занятий'))
+    markup.add(types.KeyboardButton('Изменить запись занятий'))
+    markup.add(types.KeyboardButton('Просмотреть мои записи'))
     if hello_message:
         bot.send_message(message.chat.id, "Привет✌\nЗдесь вы сможете указать актуальное расписание!")
     bot.send_message(message.chat.id, "Выберите, что бы вы хотели сделать 👇", reply_markup=markup)
 
 
-
 @bot.message_handler(content_types=['text'])
 def message_reply(message):
+    user_info = '@' + message.from_user.username \
+        if message.from_user.username is not None \
+        else (message.contact if message.contact is not None else message.from_user.full_name)
+
     if message.text == 'Записаться на занятие':
-        bot.send_message(message.chat.id, "Выберите, что бы вы хотели сделать 👇", reply_markup=weekdays_markup())
-        print(message.chat.id)
-    if message.text in ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']:
+        create_entry(message)
+
+    elif message.text == 'Изменить запись занятий':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        markup.add(types.KeyboardButton('Отменить запись'))
+        markup.add(types.KeyboardButton('Перенести запись'))
+        markup.add(types.KeyboardButton('Отмена'))
+        bot.send_message(message.chat.id, 'Выберите, что бы вы хотели сделать 👇', reply_markup=markup)
+
+    elif message.text == 'Отменить запись':
+        global mode
+        mode = Modes.CANCEL
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        try:
+            for entry in spreadsheet.get_entries_by_user(user_info)[0]:
+                markup.add((types.KeyboardButton(entry[0] + ' ' + entry[1])))
+            markup.add(types.KeyboardButton('Отмена'))
+        except spreadsheet.ZeroEntries as exc:
+            bot.send_message(message.chat.id, str(exc))
+            start_message(message, hello_message=False)
+        else:
+            bot.send_message(message.chat.id, 'Выберите запись для отмены 👇', reply_markup=markup)
+
+    elif message.text == 'Перенести запись':
+        mode = Modes.MOVE
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        try:
+            for entry in spreadsheet.get_entries_by_user(user_info)[0]:
+                markup.add((types.KeyboardButton(entry[0] + ' ' + entry[1])))
+            markup.add(types.KeyboardButton('Отмена'))
+        except spreadsheet.ZeroEntries as exc:
+            bot.send_message(message.chat.id, str(exc))
+            start_message(message, hello_message=False)
+        else:
+            bot.send_message(message.chat.id, 'Выберите запись для отмены 👇', reply_markup=markup)
+
+    elif message.text == 'Просмотреть мои записи':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        for entry in spreadsheet.get_entries_by_user(user_info)[0]:
+            bot.send_message(message.chat.id, entry[0] + ' ' + entry[1])
+        start_message(message, hello_message=False)
+
+    elif message.text in ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']:
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         global cur_weekday
         cur_weekday = message.text
-        for time in spreadsheet.get_free_time(message.text):
-            markup.add(types.KeyboardButton(time))
-        bot.send_message(message.chat.id, 'Пожалуйста, выберите удобное время 👇', reply_markup=markup)
+        try:
+            for time in spreadsheet.get_free_time(message.text):
+                markup.add(types.KeyboardButton(time))
+            markup.add(types.KeyboardButton('Отмена'))
+        except spreadsheet.ZeroEntries as exc:
+            bot.send_message(message.chat.id, str(exc))
+            start_message(message, hello_message=False)
+        else:
+            bot.send_message(message.chat.id, 'Пожалуйста, выберите удобное время 👇', reply_markup=markup)
+
     elif time_pattern.match(message.text):
         if cur_weekday is None:
             bot.send_message(message.chat.id, 'Перед выбором времени нужно выбрать день недели',
                              reply_markup=weekdays_markup())
-            return
-        user_info = '@' + message.from_user.username \
-            if message.from_user.username is not None \
-            else (message.contact if message.contact is not None else message.from_user.full_name)
-        spreadsheet.update_user_schedule(user_info
-                                         , cur_weekday
-                                         , message.text)
-        bot.send_message(message.chat.id, 'Запись успешно выполнена!')
-        print(user_info)
-        bot.send_message(997567679, user_info + ' записался на ' + message.text + ' в ' + cur_weekday)
-        cur_weekday = None
+            start_message(message, hello_message=False)
+        try:
+            spreadsheet.update_user_schedule(user_info, cur_weekday, message.text)
+
+        except spreadsheet.OccupiedEntry as exc:
+            bot.send_message(message.chat.id, str(exc))
+            start_message(message, hello_message=False)
+        else:
+            bot.send_message(message.chat.id, 'Запись успешно выполнена!')
+            print(user_info)
+            bot.send_message(997567679, user_info + ' записался на ' + message.text + ' в ' + cur_weekday)
+            cur_weekday = None
+            start_message(message, hello_message=False)
+    elif entry_pattern.match(message.text):
+        if mode is Modes.CANCEL:
+            day, time = tuple(message.text.split(' ', 1))
+            spreadsheet.update_user_schedule(user_info, day, time, delete=True)
+            bot.send_message(message.chat.id, 'Запись успешно удалена!')
+            bot.send_message(997567679, user_info + ' удалил запись в ' + day + ' ' + time)
+            start_message(message, hello_message=False)
+        elif mode is Modes.MOVE:
+            day, time = tuple(message.text.split(' ', 1))
+            spreadsheet.update_user_schedule(user_info, day, time, delete=True)
+            create_entry(message)
+    elif message.text == 'Отмена':
         start_message(message, hello_message=False)
+
 
 
 print('bot started')
--- spreadsheet.py
+++ spreadsheet.py
@@ -1,7 +1,16 @@
 import gspread
+import numpy as np
 
 import spreadsheet_consts
-from spreadsheet_consts import parse_value
+from spreadsheet_consts import parse_value, parse_days_index, parse_time_index
+
+
+class ZeroEntries(Exception):
+    pass
+
+class OccupiedEntry(Exception):
+    pass
+
 
 gc = gspread.service_account(filename='pythonbotspreadsheets-4c85514af834.json')
 spreadsheet = 'https://docs.google.com/spreadsheets/d/1N2fw5LiybpWO8TnL6zg12d2Sp9i5b3wmK2GP4KMbJCw/edit?usp=sharing'
@@ -10,8 +19,21 @@ sh = gc.open_by_url(spreadsheet)
 worksheet = sh.get_worksheet(0)
 
 
+# возвращает список всех записей(занятых/незанятых) для конкретного дня
 def get_schedule_by_day(weekday: str):
     return worksheet.get_all_values()[parse_value(weekday)][1:]
+
+
+# возвращает список с данными для каждой записи для конкретного пользователя
+def get_entries_by_user(user: str):
+    all_values = np.asarray(worksheet.get_all_values())
+    entries = np.where(all_values == user)
+    # return [[parse_days_index(entries[0][i]), parse_time_index(entries[1][i] - 1)] for i in range(len(entries[0]))]
+    indices = [[entries[0][i], entries[1][i]] for i in range(len(entries[0]))]
+    strs = [[parse_days_index(entry[0]), parse_time_index(entry[1] - 1)] for entry in indices]
+    if len(indices) == 0:
+        raise ZeroEntries("На данный момент у вас нет записей!")
+    return strs, indices
 
 
 # возвращает номера незанятых времен в таблице, в списке в коде эти номера будут на 1 меньше
@@ -21,14 +43,24 @@ def get_free_time(weekday: str):
     for time_index in range(len(weekday_times)):
         if weekday_times[time_index] == '':
             available.append(spreadsheet_consts.parse_time_index(time_index))
+    if len(available) == 0:
+        raise ZeroEntries('К сожалению, в этот день нет доступных записей 😔')
     return available
 
 
 # записывает пользователя в ячейку со свободным временем
-def update_user_schedule(user_info: str, weekday: str, time: str):
-    worksheet.update_cell(parse_value(weekday) + 1
-                          , parse_value(time) + 2
-                          , user_info)
+def update_user_schedule(user_info: str, weekday: str, time: str, delete=False):
+    if delete:
+        cell_list = worksheet.range('A1:Z50')
+        cell_list[(parse_value(weekday) + 1) * 17 + parse_value(time) + 2].value = ''
+        worksheet.update_cells(cell_list)
+    else:
+        value = worksheet.cell(parse_value(weekday) + 1, parse_value(time) + 2).value
+        if worksheet.cell(parse_value(weekday) + 1, parse_value(time) + 2).value is not None:
+            raise OccupiedEntry('Ошибка! Кто-то записался на это время быстрее вас 😔')
+        worksheet.update_cell(parse_value(weekday) + 1
+                              , parse_value(time) + 2
+                              , user_info)
 
 # if __name__ == '__main__':
-#     print(get_free_time('Понедельник'))
+#     print(get_entries_by_user('@jpuew'))
